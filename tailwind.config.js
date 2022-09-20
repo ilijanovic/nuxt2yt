0513ce68e17dd69a0072d4c316063ccbc578e869
Ilija Marijanovic <ilija.marijanovic@gmx.at>
2022-09-20 13:19:16 +0200

    fix: update purge for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -11,5 +17,6 @@ module.exports = {
       textColor: ['active'],
     },
   },
+
   plugins: [],
 }
